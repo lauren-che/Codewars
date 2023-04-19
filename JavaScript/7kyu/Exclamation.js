52b26f01e433b6f2e4f421f0c892f109b3fa8ed9
Lauren Ché <laurencconner@yahoo.com>
2023-04-18 20:03:06 -0400

    pushing up test codewars

---
+++ JavaScript/7kyu/Exclamation.js
@@ -0,0 +1,3 @@
+// https://www.codewars.com/kata/57fafb6d2b5314c839000195
+
+// Remove words from the sentence if they contain exactly one exclamation mark. Words are separated by a single space, without leading/trailing spaces.
